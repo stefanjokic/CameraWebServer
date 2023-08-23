2e5848ee55e6c41237ecf16e9dd3dd153b9874b6
stefanjokic99 <105276465+stefansingularity@users.noreply.github.com>
2023-08-23 10:23:53 +0200

    change format

--- CameraWebServer.ino
+++ CameraWebServer.ino
@@ -113,7 +113,7 @@ void setup() {
   }
   // drop down frame size for higher initial frame rate
   if(config.pixel_format == PIXFORMAT_JPEG){
-    s->set_framesize(s, FRAMESIZE_QVGA);
+    s->set_framesize(s, FRAMESIZE_XGA);
   }
 
 #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
